90b8c1d3badcc8fe8ae22ef53ad258a442309d13
mahdikarray <karraymahdi0@gmail.com>
2024-02-20 19:15:37 +0100

    commit_for_editor_js_backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EditorModule } from './editor/editor.module';
-import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest?directConnection=true'),
     EditorModule,
-    TaskModule,
   ],
 })
 export class AppModule {}
