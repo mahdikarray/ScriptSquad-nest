6fc5ec5a00580859cedc131e72b930ba9be17db9
Fayrouz Hidouri <fayrouz.hidouri@esprit.tn>
2024-02-20 20:02:36 +0100

    CRUD_DOCUMENT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { DocumentsModule } from './Document/documents.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest?directConnection=true')],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest'),
+    DocumentsModule,
+  ],
 })
 export class AppModule {}
