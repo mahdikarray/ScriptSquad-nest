0f7574c20078f0d4255eefb7cd86120c9404a662
WaelFerjani <wael.ferjani@esprit.tn>
2024-02-20 19:49:12 +0100

    first-commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import { WorkspaceModule } from './workspace/workspace.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest?directConnection=true')],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest?directConnection=true'), WorkspaceModule,
+],
+ 
 })
 export class AppModule {}
---
+++ src/workspace/workspace.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Get, Post, Put, Delete, Param, Body, NotFoundException } from '@nestjs/common';
+import { WorkspaceService } from './workspace.service';
+import { Workspace } from 'src/workspace.schema';
+
+@Controller('workspaces')
+export class WorkspaceController {
+  constructor(private readonly workspaceService: WorkspaceService) {}
+
+  @Post()
+  async create(@Body() workspace: Workspace) {
+    return await this.workspaceService.create(workspace);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.workspaceService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    try {
+      return await this.workspaceService.findOne(id);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() workspace: Workspace) {
+    try {
+      return await this.workspaceService.update(id, workspace);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    try {
+      return await this.workspaceService.remove(id);
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+}
