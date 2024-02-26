5a279bc3cea2a1dd606e734e7f3f096ac07cbffa
mahdikarray <karraymahdi0@gmail.com>
2024-02-26 21:12:07 +0100

    Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EditorModule } from './editor/editor.module';
-import { DocumentsModule } from './Document/documents.module';
 import { TaskModule } from './Task/Task.module';
 import { ChatGateway } from './chat/chat.gateway';
 
@@ -10,7 +9,6 @@ import { ChatGateway } from './chat/chat.gateway';
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest?directConnection=true'),
     EditorModule,
-    DocumentsModule,
     TaskModule,
 
   ],
