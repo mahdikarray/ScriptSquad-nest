5bfdf8ddc12f80019c5a8d9c430ca10d3d4ae05a
mahdikarray <karraymahdi0@gmail.com>
2024-02-27 18:17:16 +0100

    Commit

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors(); // This enables CORS for all origins, methods, and headers. Adjust as needed.
-  await app.listen(3000);
-}
-bootstrap();
